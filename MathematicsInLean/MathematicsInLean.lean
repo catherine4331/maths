5905efc4bd14fcd1f4be40de5ac139e9d8ddf089
catherine4331 <catherine4331@gmail.com>
2025-02-24 23:42:03 +0000

    We're gonna do mathematics in lean instead

---
+++ MathematicsInLean/MathematicsInLean.lean
@@ -0,0 +1,4 @@
+-- This module serves as the root of the `MathematicsInLean` library.
+-- Import modules here that should be built as part of the library.
+import MathematicsInLean.Introduction
+import MathematicsInLean.Common
---
+++ MathematicsInLean/MathematicsInLean/Introduction.lean
@@ -0,0 +1,10 @@
+import MathematicsInLean.Common
+
+#eval "Hello, world!"
+
+#check 2 + 2
+
+def f (x : ℕ) :=
+  x + 3
+
+#check f
