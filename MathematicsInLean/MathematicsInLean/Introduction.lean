06a51a5f995f7ceadda46ceee5dafcfdf95b1620
catherine4331 <catherine4331@gmail.com>
2025-03-18 15:45:50 +0000

    Start the intro exercises

--- MathematicsInLean/MathematicsInLean/Introduction.lean
+++ MathematicsInLean/MathematicsInLean/Introduction.lean
@@ -8,3 +8,45 @@ def f (x : ℕ) :=
   x + 3
 
 #check f
+
+#check 2 + 2 = 4
+
+def FermatLastTheorem :=
+  ∀ x y z n : ℕ, n > 2 ∧ x * y * z ≠ 0 → x ^ n + y ^ n ≠ z ^ n
+
+#check FermatLastTheorem
+
+theorem easy : 2 + 2 = 4 :=
+  rfl
+
+#check easy
+
+-- We can construct expressions directly
+example : ∀ m n : ℕ, Even n → Even (m * n) := fun m n ⟨k, (hk : n = k + k)⟩ ↦
+  have hmn : m * n = m * k + m * k := by rw [hk, mul_add]
+  show ∃ l, m * n = l + l from ⟨_, hmn⟩
+
+-- This can be condensed
+example : ∀ m n : ℕ, Even n → Even (m * n) :=
+  fun m n ⟨k, hk⟩ ↦ ⟨m * k, by rw [hk, mul_add]⟩
+
+-- We can also use a tactics style approach
+example : ∀ m n : ℕ, Even n → Even (m * n) := by
+  rintro m n ⟨k, hk⟩
+  use m * k
+  rw [hk]
+  ring
+
+example (a b c : ℝ) : a * b * c = b * (a * c) := by
+  rw [mul_comm a b]
+  rw [mul_assoc b a c]
+
+example (a b c : ℝ) : c * b * a = b * (a * c) := by
+  rw [mul_comm c b]
+  rw [mul_comm a c]
+  rw [mul_assoc b c a]
+
+example (a b c : ℝ) : a * (b * c) = b * (a * c) := by
+  rw [← mul_assoc a b c]
+  rw [mul_comm a b]
+  rw [mul_assoc b a c]
