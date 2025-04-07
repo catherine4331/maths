72fd079217d7a8709b5ab2dde6d60dec5540602e
catherine4331 <catherine4331@gmail.com>
2025-04-07 23:55:41 +0100

    Move stuff from incoming branch to where it should be

--- MathematicsInLean/MathematicsInLean.lean
+++ MathematicsInLean/MathematicsInLean.lean
@@ -1,8 +1,9 @@
 -- This module serves as the root of the `MathematicsInLean` library.
 -- Import modules here that should be built as part of the library.
-import MathematicsInLean.Introduction
-import MathematicsInLean.Basics.TheoremsAndLemmas
-import MathematicsInLean.Basics.Basics
 import MathematicsInLean.Common
-import MathematicsInLean.Apply
-import MathematicsInLean.Logic
+import MathematicsInLean.Basics.Introduction
+import MathematicsInLean.Basics.Apply
+import MathematicsInLean.Basics.Basics
+import MathematicsInLean.Basics.Structure
+import MathematicsInLean.Basics.TheoremsAndLemmas
+import MathematicsInLean.Logic.Implication
--- MathematicsInLean/MathematicsInLean/Basics/TheoremsAndLemmas.lean
+++ MathematicsInLean/MathematicsInLean/Basics/TheoremsAndLemmas.lean
@@ -122,14 +122,4 @@ example : 2*a*b ≤ a^2 + b^2 := by
     _ ≥ 0 := by apply pow_two_nonneg
   linarith
 
-example : |a*b| ≤ (a^2 + b^2)/2 := by
-  apply abs_le'.mpr
-  constructor
-  . have h : 0 ≤ a^2 + b^2 - 2*a*b
-    calc
-      a^2 - 2*a*b + b^2 = (a - b)^2 := by ring
-      _ ≥ 0 := by apply pow_two_nonneg
-    linarith
-
-
 #check abs_le'.mpr
