536925728c63190fdc8d56fc9d5e522cfcfe2780
Catherine Reid <catherine.reid@neo4j.com>
2025-04-17 09:28:15 +0100

    Finish up conjunction

--- MathematicsInLean/MathematicsInLean/Logic/Conjuction.lean
+++ MathematicsInLean/MathematicsInLean/Logic/Conjuction.lean
@@ -46,3 +46,66 @@ example {m n : ℕ} (h : m ∣ n ∧ m ≠ n) : m ∣ n ∧ ¬n ∣ m := by
   · intro h₃
     apply h₂
     exact Nat.dvd_antisymm h₁ h₃
+
+
+theorem aux {x y : ℝ} (h : x ^ 2 + y ^ 2 = 0) : x = 0 :=
+  have h' : x ^ 2 = 0 := by
+    have h₁ : 0 ≤ y ^ 2 := pow_two_nonneg y
+    have h₂ : 0 ≤ x ^ 2 := pow_two_nonneg x
+    linarith
+  pow_eq_zero h'
+
+example (x y : ℝ) : x ^ 2 + y ^ 2 = 0 ↔ x = 0 ∧ y = 0 := by
+  constructor
+  · intro h
+    constructor
+    · apply aux h
+    · rw [add_comm] at h
+      apply aux h
+  · rintro ⟨rfl, rfl⟩
+    -- intro h
+    -- rw [h.left, h.right]
+    norm_num
+
+example (x : ℝ) : |x + 3| < 5 → -8 < x ∧ x < 2 := by
+  rw [abs_lt]
+  intro h
+  constructor <;> linarith
+
+example : 3 ∣ Nat.gcd 6 15 := by
+  rw [Nat.dvd_gcd_iff]
+  constructor <;> norm_num
+
+theorem not_monotone_iff {f : ℝ → ℝ} : ¬Monotone f ↔ ∃ x y, x ≤ y ∧ f x > f y := by
+  rw [Monotone]
+  push_neg
+  rfl
+
+example : ¬Monotone fun x : ℝ ↦ -x := by
+  rw [Monotone]
+  push_neg
+  use 1, 2
+  constructor <;> norm_num
+
+section
+
+variable {α : Type*} [PartialOrder α]
+variable (a b : α)
+
+example : a < b ↔ a ≤ b ∧ a ≠ b := by
+  rw [lt_iff_le_not_le]
+  constructor
+  · rintro ⟨h₁, h₂⟩
+    constructor
+    · exact h₁
+    · intro h₃
+      apply h₂
+      rw [h₃]
+  · rintro ⟨h₁, h₂⟩
+    constructor
+    · exact h₁
+    · intro h₃
+      apply h₂
+      apply le_antisymm h₁ h₃
+
+end
