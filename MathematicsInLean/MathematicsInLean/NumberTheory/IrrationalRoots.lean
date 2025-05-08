452ab00b5e7319286104c44a3e741e15207380b9
Catherine Reid <catherine.reid@neo4j.com>
2025-05-08 14:42:42 +0100

    I'm very tired

---
+++ MathematicsInLean/MathematicsInLean/NumberTheory/IrrationalRoots.lean
@@ -0,0 +1,134 @@
+import MathematicsInLean.Common
+import Mathlib.Data.Nat.Factorization.Basic
+import Mathlib.Data.Nat.Prime.Basic
+
+example (m n : Nat) (h : m.Coprime n) : m.gcd n = 1 :=
+  h
+
+example (m n : Nat) (h : m.Coprime n) : m.gcd n = 1 := by
+  rw [Nat.Coprime] at h
+  exact h
+
+example : Nat.Coprime 12 7 := by norm_num
+
+example : Nat.gcd 12 8 = 4 := by norm_num
+
+example (p : ℕ) (prime_p : Nat.Prime p) : 2 ≤ p ∧ ∀ m : ℕ, m < p → m ∣ p → m = 1 := by
+  rwa [Nat.prime_def_lt] at prime_p
+
+example (p : ℕ) (prime_p : Nat.Prime p) : ∀ m : ℕ, m ∣ p → m = 1 ∨ m = p :=
+  prime_p.eq_one_or_self_of_dvd
+
+#check Nat.Prime.dvd_mul
+
+theorem even_of_even_sqr {m : ℕ} (h : 2 ∣ m^2) : 2 ∣ m := by
+  rw [pow_two, Nat.prime_two.dvd_mul] at h
+  cases h <;> assumption
+
+example (a b c : Nat) (h : a * b = a * c) (h' : a ≠ 0) : b = c :=
+  -- apply? suggests the following:
+  (mul_right_inj' h').mp h
+
+example {m n : ℕ} (coprime_mn : m.Coprime n) : m ^ 2 ≠ 2 * n ^ 2 := by
+  intro sqr_eq
+  have : 2 ∣ m := by
+    apply even_of_even_sqr
+    use n^2
+  obtain ⟨k, meq⟩ := dvd_iff_exists_eq_mul_left.mp this
+  have : 2 * (2 * k ^ 2) = 2 * n ^ 2 := by
+    rw [← sqr_eq, meq]
+    ring
+  have : 2 * k ^ 2 = n ^ 2 := by
+    rw [mul_right_inj'] at this
+    exact this
+    norm_num
+  have : 2 ∣ n := by
+    apply even_of_even_sqr
+    rw [← this]
+    apply dvd_mul_right
+  have : 2 ∣ m.gcd n := by
+    apply Nat.dvd_gcd <;> assumption
+  have : 2 ∣ 1 := by
+    convert this
+    symm
+    exact coprime_mn
+  norm_num at this
+
+-- We can prove this for any prime without too much extra difficulty
+example {m n p : ℕ} (coprime_mn : m.Coprime n) (prime_p : p.Prime) : m ^ 2 ≠ p * n ^ 2 := by
+  intro sqr_eq
+  have : p ∣ m := by
+    apply prime_p.dvd_of_dvd_pow
+    rw [sqr_eq]
+    apply dvd_mul_right
+  obtain ⟨k, meq⟩ := dvd_iff_exists_eq_mul_left.mp this
+  have : p * (p * k^2) = p * n^2 := by
+    rw [← sqr_eq, meq]
+    ring
+  have : p * k ^ 2 = n ^ 2 := by
+    rw [mul_right_inj'] at this
+    exact this
+    exact Nat.Prime.ne_zero prime_p
+  have : p ∣ n := by
+    apply prime_p.dvd_of_dvd_pow
+    rw [← this]
+    apply dvd_mul_right
+  have : p ∣ m.gcd n := by
+    apply Nat.dvd_gcd <;> assumption
+  have : p ∣ 1 := by
+    convert this
+    symm
+    apply coprime_mn
+  have : 2 ≤ 1 := by
+    apply prime_p.two_le.trans
+    exact Nat.le_of_dvd zero_lt_one this
+  norm_num at this
+
+#check Nat.primeFactorsList
+#check Nat.prime_of_mem_primeFactorsList
+#check Nat.prod_primeFactorsList
+#check Nat.primeFactorsList_unique
+
+theorem factorization_mul' {m n : ℕ} (mnez : m ≠ 0) (nnez : n ≠ 0) (p : ℕ) :
+    (m * n).factorization p = m.factorization p + n.factorization p := by
+  rw [Nat.factorization_mul mnez nnez]
+  rfl
+
+theorem factorization_pow' (n k p : ℕ) :
+    (n ^ k).factorization p = k * n.factorization p := by
+  rw [Nat.factorization_pow]
+  rfl
+
+theorem Nat.Prime.factorization' {p : ℕ} (prime_p : p.Prime) :
+    p.factorization p = 1 := by
+  rw [prime_p.factorization]
+  simp
+
+example {m n p : ℕ} (nnz : n ≠ 0) (prime_p : p.Prime) : m ^ 2 ≠ p * n ^ 2 := by
+  intro sqr_eq
+  have nsqr_nez : n ^ 2 ≠ 0 := by simpa
+  have eq1 : Nat.factorization (m ^ 2) p = 2 * m.factorization p := by
+    rw [factorization_pow']
+  have eq2 : (p * n ^ 2).factorization p = 2 * n.factorization p + 1 := by
+    rw [factorization_mul' prime_p.ne_zero nsqr_nez, factorization_pow', prime_p.factorization']
+    ring
+  have : 2 * m.factorization p % 2 = (2 * n.factorization p + 1) % 2 := by
+    rw [← eq1, sqr_eq, eq2]
+  rw [add_comm, Nat.add_mul_mod_self_left, Nat.mul_mod_right] at this
+  norm_num at this
+
+example {m n k r : ℕ} (nnz : n ≠ 0) (pow_eq : m ^ k = r * n ^ k) {p : ℕ} :
+    k ∣ r.factorization p := by
+  rcases r with _ | r
+  · simp
+  have npow_nz : n ^ k ≠ 0 := fun npowz ↦ nnz (pow_eq_zero npowz)
+  have eq1 : (m ^ k).factorization p = k * m.factorization p := by
+    exact factorization_pow' m k p
+  have eq2 : ((r + 1) * n ^ k).factorization p =
+      k * n.factorization p + (r + 1).factorization p := by
+    rw [factorization_mul' r.succ_ne_zero npow_nz, add_comm, factorization_pow']
+  have : r.succ.factorization p = k * m.factorization p - k * n.factorization p := by
+    rw [← eq1, pow_eq, eq2, add_comm, Nat.add_sub_cancel]
+  rw [this]
+  apply Nat.dvd_sub <;>
+  · apply Nat.dvd_mul_right
--- MathematicsInLean/MathematicsInLean/SetsFunctions/SchroderBernstein.lean
+++ MathematicsInLean/MathematicsInLean/SetsFunctions/SchroderBernstein.lean
@@ -1,2 +1,37 @@
 import MathematicsInLean.Common
-import Mathlib.Data.Real.Basic
+import Mathlib.Data.Set.Lattice
+import Mathlib.Data.Set.Function
+
+open Set
+open Function
+
+noncomputable section
+open Classical
+variable {α β : Type*} [Nonempty β]
+
+section
+
+variable (f : α → β) (g : β → α)
+
+variable (f : α → β) (g : β → α)
+
+def sbAux : ℕ → Set α
+  | 0 => univ \ g '' univ
+  | n + 1 => g '' (f '' sbAux n)
+
+def sbSet :=
+  ⋃ n, sbAux f g n
+
+def sbFun (x : α) : β :=
+  if x ∈ sbSet f g then f x else invFun g x
+
+end
+section
+variable (g : β → α) (x : α)
+
+#check (invFun g : α → β)
+#check (leftInverse_invFun : Injective g → LeftInverse (invFun g) g)
+#check (leftInverse_invFun : Injective g → ∀ y, invFun g (g y) = y)
+#check (invFun_eq : (∃ y, g y = x) → g (invFun g x) = x)
+
+end
