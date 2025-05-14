9fea39874736b3bc2ec23df00892cae0835a4c24
catherine4331 <catherine4331@gmail.com>
2025-05-14 18:24:58 +0100

    Proove every natural number ge two has a prime factor

--- MathematicsInLean/MathematicsInLean/NumberTheory/InfinitePrimes.lean
+++ MathematicsInLean/MathematicsInLean/NumberTheory/InfinitePrimes.lean
@@ -16,3 +16,21 @@ example {m : ℕ} (h0 : m ≠ 0) (h1 : m ≠ 1) : 2 ≤ m := by
   revert h0 h1
   revert h m
   decide
+
+theorem exists_prime_factor {n : Nat} (h : 2 ≤ n) : ∃ p : Nat, p.Prime ∧ p ∣ n := by
+  by_cases np : n.Prime
+  · use n
+  induction' n using Nat.strong_induction_on with n ih
+  rw [Nat.prime_def_lt] at np
+  push_neg at np
+  rcases np h with ⟨m, mltn, mdvdn, mne1⟩
+  have : m ≠ 0 := by
+    intro mz
+    rw [mz, zero_dvd_iff] at mdvdn
+    linarith
+  have mgt2 : 2 ≤ m := two_le this mne1
+  by_cases mp : m.Prime
+  · use m
+  · rcases ih m mltn mgt2 mp with ⟨p, pp, pdvd⟩
+    use p, pp
+    apply pdvd.trans mdvdn
