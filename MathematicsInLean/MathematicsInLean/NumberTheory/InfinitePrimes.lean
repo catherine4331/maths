2f160906baba22c849718cf62f4dfd505316fe64
catherine4331 <catherine4331@gmail.com>
2025-05-19 15:47:23 +0100

    Another proof of the infinity of primes

--- MathematicsInLean/MathematicsInLean/NumberTheory/InfinitePrimes.lean
+++ MathematicsInLean/MathematicsInLean/NumberTheory/InfinitePrimes.lean
@@ -34,3 +34,105 @@ theorem exists_prime_factor {n : Nat} (h : 2 ≤ n) : ∃ p : Nat, p.Prime ∧ p
   · rcases ih m mltn mgt2 mp with ⟨p, pp, pdvd⟩
     use p, pp
     apply pdvd.trans mdvdn
+
+theorem primes_infinite : ∀ n, ∃ p > n, Nat.Prime p := by
+  intro n
+  have : 2 ≤ Nat.factorial n + 1 := by
+    simp
+    rw [Nat.one_le_iff_ne_zero]
+    apply Nat.factorial_ne_zero
+  rcases exists_prime_factor this with ⟨p, pp, pdvd⟩
+  refine ⟨p, ?_, pp⟩
+  show p > n
+  by_contra ple
+  push_neg at ple
+  have : p ∣ Nat.factorial n := by
+    apply Nat.dvd_factorial (Nat.Prime.pos pp) ple
+  have : p ∣ 1 := by
+    rw [← Nat.dvd_add_right this]
+    exact pdvd
+  show False
+  rw [Nat.dvd_one] at this
+  linarith [pp.two_le]
+
+open Finset
+
+section
+
+variable {α : Type*} [DecidableEq α] (r s t : Finset α)
+
+example : r ∩ (s ∪ t) ⊆ r ∩ s ∪ r ∩ t := by
+  rw [subset_iff]
+  intro x
+  rw [mem_inter, mem_union, mem_union, mem_inter, mem_inter]
+  tauto
+
+example : r ∩ s ∪ r ∩ t ⊆ r ∩ (s ∪ t) := by
+  simp [subset_iff]
+  intro x
+  tauto
+
+example : (r ∪ s) ∩ (r ∪ t) = r ∪ s ∩ t := by
+  ext x
+  simp
+  tauto
+
+example : (r \ s) \ t = r \ (s ∪ t) := by
+  ext x
+  simp
+  tauto
+
+example (s : Finset ℕ) (n : ℕ) (h : n ∈ s) : n ∣ ∏ i ∈ s, i :=
+  Finset.dvd_prod_of_mem _ h
+
+theorem _root_.Nat.Prime.eq_of_dvd_of_prime {p q : ℕ}
+  (prime_p : Nat.Prime p) (prime_q : Nat.Prime q) (h : p ∣ q) :
+  p = q := by
+    apply Nat.Prime.eq_one_or_self_of_dvd prime_q at h
+    rcases h with h | h
+    · apply Nat.Prime.ne_one at prime_p
+      contradiction
+    · exact h
+
+theorem mem_of_dvd_prod_primes {s : Finset ℕ} {p : ℕ} (prime_p : p.Prime) :
+    (∀ n ∈ s, Nat.Prime n) → (p ∣ ∏ n ∈ s, n) → p ∈ s := by
+  intro h₀ h₁
+  induction' s using Finset.induction_on with a s ans ih
+  · simp at h₁
+    linarith [prime_p.two_le]
+  · simp [Finset.prod_insert ans, prime_p.dvd_mul] at h₀ h₁
+    rw [mem_insert]
+    rcases h₁ with l | r
+    · exact Or.inl (Nat.Prime.eq_of_dvd_of_prime prime_p h₀.left l)
+    · exact Or.inr (ih h₀.right r)
+
+example (s : Finset ℕ) (x : ℕ) : x ∈ s.filter Nat.Prime ↔ x ∈ s ∧ x.Prime :=
+  mem_filter
+
+theorem primes_infinite' : ∀ s : Finset Nat, ∃ p, Nat.Prime p ∧ p ∉ s := by
+  intro s
+  by_contra h
+  push_neg at h
+  set s' := s.filter Nat.Prime with s'_def
+  have mem_s' : ∀ {n : ℕ}, n ∈ s' ↔ n.Prime := by
+    intro n
+    simp [s'_def]
+    apply h
+  have : 2 ≤ (∏ i ∈ s', i) + 1 := by
+    simp
+    apply Nat.succ_le_of_lt
+    apply Finset.prod_pos
+    intro n ns'
+    apply (mem_s'.mp ns').pos
+  rcases exists_prime_factor this with ⟨p, pp, pdvd⟩
+  have : p ∣ ∏ i ∈ s', i := by
+    apply dvd_prod_of_mem
+    rw [mem_s']
+    exact pp
+  have : p ∣ 1 := by
+    convert Nat.dvd_sub pdvd this
+    simp
+  apply Nat.Prime.not_dvd_one pp at this
+  assumption
+
+end
