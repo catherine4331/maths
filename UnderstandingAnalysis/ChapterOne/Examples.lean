04808c32863df5ba07ef5b58f3ea67d4797e97b9
Catherine Reid <catherine.reid@neo4j.com>
2025-05-31 23:07:35 +0100

    Actually making some progress!

--- UnderstandingAnalysis/ChapterOne/Examples.lean
+++ UnderstandingAnalysis/ChapterOne/Examples.lean
@@ -3,6 +3,23 @@ import Mathlib.Util.Delaborators
 
 open Set
 
+example (a b : ℝ) : (∀ ε > 0, |a - b| < ε) → a = b := by
+  intro h
+  by_contra hab
+  have h_pos : |a - b| > 0 := abs_pos.mpr (sub_ne_zero.mpr hab)
+  let ε₀ := |a - b|
+  have hε_pos : ε₀ > 0 := by
+    apply abs_pos.mpr
+    exact sub_ne_zero_of_ne hab
+  have h_lt : |a - b| < ε₀ := by
+    apply h
+    apply hε_pos
+  have: |a - b| < |a - b| := by calc
+    |a - b| < ε₀ := by apply h_lt
+            _ = |a - b| := by ring
+  rw [← lt_self_iff_false |a - b|]
+  exact this
+
 example {α : Type*} (A B : Set α) : (A ∩ B)ᶜ = Aᶜ ∪ Bᶜ := by
   rw [← compl_compl (Aᶜ ∪ Bᶜ)]
   rw [compl_union]
