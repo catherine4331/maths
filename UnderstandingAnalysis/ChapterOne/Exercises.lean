6396e66bd29bd2d2c76bb85b23009fe5d2617c71
Catherine Reid <catherine.reid@neo4j.com>
2025-06-01 13:23:28 +0100

    Do the reverse triangle inequality

--- UnderstandingAnalysis/ChapterOne/Exercises.lean
+++ UnderstandingAnalysis/ChapterOne/Exercises.lean
@@ -22,3 +22,24 @@ theorem triangle (a b : ℝ) : |a + b| ≤ |a| + |b| := by
   apply this
   apply abs_nonneg
   apply add_nonneg (abs_nonneg a) (abs_nonneg b)
+
+example (a b c d : ℝ) : |a - b| ≤ |a - c| + |c - d| + |d - b| := by calc
+  |a - b| = |(a - c) + (c - b)| := by ring_nf
+        _ ≤ |a - c| + |c - b| := triangle (a - c) (c - b)
+        _ = |a - c| + |(c - d) + (d - b)| := by ring_nf
+        _ ≤ |a - c| + |c - d| + |d - b| := by
+          rw [add_assoc, add_le_add_iff_left]
+          apply triangle
+
+theorem reverse_triangle (a b : ℝ) : |(|a| - |b|)| ≤ |a - b| := by
+  have : |a| ≤ |a - b| + |b| := by calc
+    |a| = |a - b + b| := by ring_nf
+      _ ≤ |a - b| + |b| := triangle (a - b) b
+  have pos : |a| - |b| ≤ |a - b| := by linarith
+  have : |b| ≤ |a - b| + |a| := by calc
+    |b| = |b - a + a| := by ring_nf
+      _ ≤ |b - a| + |a| := triangle (b - a) a
+      _ = |a - b| + |a| := by rw [abs_sub_comm]
+  have neg : -|a - b| ≤ |a| - |b| := by linarith
+  rw [abs_le]
+  exact And.intro neg pos
