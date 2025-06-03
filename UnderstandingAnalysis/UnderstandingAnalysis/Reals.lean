a990f47614c9b34f5c4f1133d5d3cbda4ccd69f2
catherine4331 <catherine4331@gmail.com>
2025-06-03 16:26:06 +0100

    Wrap up the sup & inf analytic lemmas

--- UnderstandingAnalysis/UnderstandingAnalysis/Reals.lean
+++ UnderstandingAnalysis/UnderstandingAnalysis/Reals.lean
@@ -7,25 +7,70 @@ def BoundedAbove (A : Set ℝ) : Prop :=
 def BoundedBelow (A : Set ℝ) : Prop :=
   ∃ l : ℝ, ∀ a ∈ A, l ≤ a
 
+@[simp]
 def UpperBound (A : Set ℝ) (s : ℝ) : Prop :=
   ∀ a ∈ A, a ≤ s
 
+@[simp]
 def LowerBound (A : Set ℝ) (i : ℝ) : Prop :=
   ∀ a ∈ A, i ≤ a
 
+@[simp]
 def Supremum (A : Set ℝ) (s : ℝ) : Prop :=
   UpperBound A s ∧ ∀ b : ℝ, UpperBound A b → s ≤ b
 
+@[simp]
 def Infimum (A : Set ℝ) (i : ℝ) : Prop :=
   LowerBound A i ∧ ∀ l : ℝ, LowerBound A l → l ≤ i
 
+@[simp]
 def Maximum (A : Set ℝ) (a₀ : A) : Prop :=
   ∀ a ∈ A, a₀ ≥ a
 
+@[simp]
 def Minimum (A : Set ℝ) (a₁ : A) : Prop :=
   ∀ a ∈ A, a₁ ≤ a
 
-lemma sup_eps {A : Set ℝ} (h : UpperBound A s) :
+lemma sup_analytic {A : Set ℝ} (h : UpperBound A s) :
     Supremum A s ↔ ∀ ε > 0, ∃ a : A, s - ε < a := by
+  simp
   constructor
-  ·
+  · rintro h ε ε_pos
+    have sε : s - ε < s := by linarith
+    have := mt (h.right (s - ε))
+    push_neg at this
+    rcases this sε with ⟨a, ha⟩
+    use a
+  · rintro hanalytic
+    constructor
+    · exact h
+    · rintro b b_ub
+      by_contra h_not_le
+      have b_lt_s : b < s := lt_of_not_le h_not_le
+      obtain ⟨a, ha_mem, ha_gt⟩ := hanalytic (s - b) (by linarith)
+      have : b < a := by linarith
+      -- Ask about tidying this up
+      apply lt_irrefl b
+      apply this.trans_le
+      apply b_ub
+      apply ha_mem
+
+lemma inf_analytic {A : Set ℝ} (h : LowerBound A i) :
+    Infimum A i ↔ ∀ ε > 0, ∃ a : A, a < i + ε := by
+  simp
+  constructor
+  · rintro h ε ε_pos
+    have iε : i < i + ε := by linarith
+    have := mt (h.right (i + ε))
+    push_neg at this
+    rcases this iε with ⟨a, ha⟩
+    use a
+  · rintro hanalytic
+    constructor
+    · exact h
+    · rintro l l_lb
+      by_contra h_not_le
+      have i_lt_l : i < l := lt_of_not_ge h_not_le
+      obtain ⟨a, ha_mem, ha_lt⟩ := hanalytic (l - i) (by linarith)
+      have : a < l := by linarith
+      apply lt_irrefl l (lt_of_le_of_lt (l_lb a ha_mem) this)
